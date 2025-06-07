297034ef024836457638ac7f9fe986a1da0939d6
govind156 <govindmadaan156@gmail.com>
2025-06-07 21:39:39 +0530

    route in updated auth.js

--- client/src/apiCalls/auth.js
+++ client/src/apiCalls/auth.js
@@ -3,7 +3,7 @@ import { axiosInstance,url } from "./index";
 
 export const signupUser=async(user)=>{
     try{
-        const response =await axiosInstance.post(url+'/api/auth/signup',user)
+        const response =await axiosInstance.post(url+'api/auth/signup',user)
         return response.data
     }catch(error){
         return error;
@@ -11,7 +11,7 @@ export const signupUser=async(user)=>{
 }
 export const verifysignup=async(verificationcode)=>{
     try{
-        const response =await axiosInstance.post(url+'/api/auth/verifysignup',verificationcode)
+        const response =await axiosInstance.post(url+'api/auth/verifysignup',verificationcode)
         return response.data
     }
     catch(error){
@@ -22,7 +22,7 @@ export const verifysignup=async(verificationcode)=>{
 
 export const logInUser=async(user)=>{
     try{
-        const response=await axiosInstance.post(url+'/api/auth/login',user)
+        const response=await axiosInstance.post(url+'api/auth/login',user)
         return response.data
     }
     catch(error){
