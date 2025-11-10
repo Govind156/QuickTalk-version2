8c9ceb2e46b46edfaeb8f1852c9ae3d83dda606e
govind156 <govindmadaan156@gmail.com>
2025-11-10 15:41:59 +0530

    change env var  to uppercase

--- server/middlewares/sendtransporter.js
+++ server/middlewares/sendtransporter.js
@@ -5,8 +5,8 @@ const transporter=nodemailer.createTransport({
     port:587,
     secure:false,
     auth:{
-       user:process.env.user,
-       pass:process.env.pass
+       user:process.env.USER,
+       pass:process.env.PASS
     }
 })
 
