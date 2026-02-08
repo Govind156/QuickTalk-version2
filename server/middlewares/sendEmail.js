fae97da981076e8ba76b3f0dacfea0d8481d929c
govind156 <govindmadaan156@gmail.com>
2026-02-08 13:15:14 +0530

    update to nodemailer

--- server/middlewares/sendEmail.js
+++ server/middlewares/sendEmail.js
@@ -1,14 +1,21 @@
 require('dotenv').config({path:'../config.env'})
 const { Welcome_Email_Template ,Verification_Email_Template} = require("./EmailTemplate");
-// const {transporter}=require("./sendtransporter")
+const {transporter}=require("./sendtransporter")
 const {Resend} = require('resend')
 const resend=new Resend(process.env.RESEND_API_KEY)
 
 
 const Sendverificationcode=async(email,verificationcode)=>{
     try{
-        const response=await resend.emails.send({
-            from:`QuickTalk <QuickTalk@resend.dev>`,
+        // const response=await resend.emails.send({
+        //     from:`QuickTalk <QuickTalk@resend.dev>`,
+        //     to:email,
+        //     subject:"verify your code",
+        //     text:"verify your code",
+        //     html:Verification_Email_Template.replace("{verificationCode}",verificationcode),
+        // })
+         const response=await transporter.sendMail({
+            from:process.env.USER,
             to:email,
             subject:"verify your code",
             text:"verify your code",
@@ -24,8 +31,15 @@ const Sendverificationcode=async(email,verificationcode)=>{
 }
 const WelcomeEmail=async(email,name)=>{
     try{
-        const response=await resend.emails.send({
-            from:`QuickTalk <QuickTalk@resend.dev>`,
+        // const response=await resend.emails.send({
+        //     from:`QuickTalk <QuickTalk@resend.dev>`,
+        //     to:email,
+        //     subject:"Welcome to QuickTalk – Let’s Get Chatting! 🚀",
+        //     text:"Welcome",
+        //     html:Welcome_Email_Template.replace("{name}",name),
+        // })
+         const response=await transporter.sendMail({
+            from:process.env.USER,
             to:email,
             subject:"Welcome to QuickTalk – Let’s Get Chatting! 🚀",
             text:"Welcome",
