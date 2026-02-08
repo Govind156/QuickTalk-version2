e2700c259305e15b0bbff3222cea17e1b8f7e4a0
govind156 <govindmadaan156@gmail.com>
2026-02-08 14:22:57 +0530

    change in transporter

--- server/middlewares/sendtransporter.js
+++ server/middlewares/sendtransporter.js
@@ -1,15 +1,22 @@
 require('dotenv').config({path:'../config.env'})
 const nodemailer=require('nodemailer')
 
-const transporter=nodemailer.createTransport({
-    host:"smtp.gmail.com",
-    port:587,
-    secure:true,
-    auth:{
-       user:process.env.USER,
-       pass:process.env.PASS
-    }
-})
+// const transporter=nodemailer.createTransport({
+//     host:"smtp.gmail.com",
+//     port:587,
+//     secure:false,
+//     auth:{
+//        user:process.env.USER,
+//        pass:process.env.PASS
+//     }
+// })
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASS
+  }
+});
 
 module.exports={transporter}
 //before port:587
